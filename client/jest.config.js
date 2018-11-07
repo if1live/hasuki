863f1880eb2ba4eb754a00a1feaafc7523da0271
if1live <libsora25@gmail.com>
2018-11-07 20:22:55 +0900

    add client test

---
+++ client/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+};
--- scripts/ci-client.sh
+++ scripts/ci-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+yarn run jest
 yarn run build
