45b9382e45ab0dfa07ac9757f9f6d78de62e70ac
if1live <libsora25@gmail.com>
2024-01-08 23:37:09 +0900

    fetch + corsproxy

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -14,6 +14,24 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 );
 */
 
+const fetch_base = globalThis.fetch;
+/**
+ * https://corsproxy.io/ 써서 cors 우회
+ * 나중에 corsproxy.io 막히면 프록시만 교체하면 될듯
+ */
+const fetch_next: typeof fetch_base = async (url, init) => {
+  if (typeof url === "string") {
+    const urlObj = new URL(url);
+    if (urlObj.hostname === "www.youtube.com") {
+      const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+      return await fetch_base(proxyUrl, init);
+    }
+  }
+  // else
+  return await fetch_base(url, init);
+};
+globalThis.fetch = fetch_next as typeof fetch_base;
+
 // https://stackoverflow.com/a/71982736
 // useEffect 2번 호출되는거 막는 더 좋은 방법을 모르겠다.
 ReactDOM.createRoot(document.getElementById("root")!).render(
