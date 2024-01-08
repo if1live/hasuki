8cff12b9f32544c7caa586769c90a4070c0298bb
if1live <libsora25@gmail.com>
2024-01-08 23:39:58 +0900

    build test

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "semantic-ui-react";
 import "./App.css";
+import { IndexPage } from "./IndexPage";
 import hasukiLogo from "./assets/hero.webp";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <Container text>
       <h1>hasuki</h1>
       <img src={hasukiLogo} className="ui large image" alt="hasuki" />
+
+      <IndexPage />
     </Container>
   );
 }
