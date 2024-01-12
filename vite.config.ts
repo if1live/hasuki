bc1413afcff4cb1bc59a21c55b25183188efb3ff
if1live <libsora25@gmail.com>
2024-01-13 01:21:02 +0900

    쓸모없는 로그 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import { defineConfig } from "vite";
 const baseUrl_ghpage = "/hasuki/";
 const baseUrl_vercel = undefined;
 const baseUrl = process.env.VERCEL ? baseUrl_vercel : baseUrl_ghpage;
-console.log("env", process.env);
-console.log("baseUrl", baseUrl);
 
 // https://vitejs.dev/config/
 export default defineConfig({
