8eb8f77dc73b641da25a23240746b9058c77e004
if1live <libsora25@gmail.com>
2024-01-13 01:16:57 +0900

    base url test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const baseUrl_ghpage = "/hasuki/";
+const baseUrl_vercel = undefined;
+const baseUrl = process.env.VERCEL ? baseUrl_vercel : baseUrl_ghpage;
+console.log("env", process.env);
+console.log("baseUrl", baseUrl);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hasuki/",
+  base: baseUrl,
 });
